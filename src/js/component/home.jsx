956dcfbc29706fe4c748f5e433255bbb25a9808e
Alex Perez Rubio <71727357+alex5perez@users.noreply.github.com>
2024-11-15 20:17:11 +0000

    [Subida completa de codigo] ---Home.jsx: ✅ Use State para saber si esta seleccionado. ✅ Use State para el boton lila. ✅ Estructura condicional para cambiar los colores con un boton. ✅ Cada boton con su emoji y sus estilos. ✅ Añadimos el selected al background y al OnClick de cada boton. ✅ 2 Botones para las peticiones bonus con sus OnClick y llamadas respectivas.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [selected, setSelected] = useState(null);
+	const [newButton, setNewButton] = useState(false);
+
+	const cambiarColor = () => {
+		if (selected === "null") {
+			setSelected("red");
+		} else if (selected === "red") {
+			setSelected("orange");
+		} else if (selected === "orange") {
+			setSelected("green");
+		} else if (selected === "green") {
+			setSelected("red");
+		}
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-1">
+
+			<div className="red"><button style={{
+				fontSize: "4rem",
+				backgroundColor: selected === "red" ? "red" : null,
+			}}
+				onClick={() => {
+					setSelected(selected === "red" ? null : "red");
+				}}>🔴</button></div>
+
+			<div className="orange"><button style={{
+				fontSize: "4rem",
+				backgroundColor: selected === "orange" ? "orange" : null,
+			}}
+				onClick={() => {
+					setSelected(selected === "orange" ? null : "orange");
+				}}>🟠</button></div>
+
+			<div className="green"><button style={{
+				fontSize: "4rem",
+				backgroundColor: selected === "green" ? "green" : null,
+			}}
+				onClick={() => {
+					setSelected(selected === "green" ? null : "green");
+				}}>🟢</button></div>
+
+			<div className="purple"><button style={{
+				fontSize: "4rem",
+				backgroundColor: selected === "purple" ? "purple" : null,
+				display: newButton ? "inline" : "none",
+			}}
+				onClick={() => {
+					setSelected(selected === "purple" ? null : "purple");
+				}}>🟣</button></div>
+
+			<div className="d-flex flex-column align-items-center">
+				<button className="btn btn-success mt-2 d-block" onClick={() => setNewButton(!newButton)}>Crea un semáforo lila</button>
+				<button className="btn btn-success mt-2 d-block" onClick={() => {setSelected("red"); cambiarColor()}}>Cambia el semáforo seleccionado</button>
+			</div>
 		</div>
 	);
 };
